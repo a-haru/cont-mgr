93a1f99e4ce9cc583b6c727819d123cccfcccbe0
a-haru <aharu.dev@gmail.com>
2020-10-02 02:02:21 +0900

    記事一覧のファイル追加のみ

---
+++ src/resources/js/components/pages/ContentList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>test</div>
+</template>
+
+<style lang="scss">
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    created()
+    {
+        console.log(this.$route.params)
+        this.fetchData();
+    },
+    methods: {
+        fetchData(): void
+        {
+            const id = parseInt(this.$route.params.clientId);
+            console.log(id);
+        },
+    }
+});
+</script>
--- src/resources/js/config/router.ts
+++ src/resources/js/config/router.ts
@@ -5,10 +5,12 @@ Vue.use(VueRouter);
 // import ClientList from '../components/pages/ClientList.vue';
 import ClientList from '../components/pages/ClientList';
 import ClientEdit from '../components/pages/ClientEdit';
+import ContentList from '../components/pages/ContentList';
 
 export default new VueRouter({
     routes: [
         {name: 'client.list', path: '/clients', component: ClientList},
-        {name: 'client.edit', path: '/client/:id/edit', component: ClientEdit}
+        {name: 'client.edit', path: '/client/:id/edit', component: ClientEdit},
+        {name: 'content.list', path: '/contents/:clientId', component: ContentList},
     ]
 });
